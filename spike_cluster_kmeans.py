4424227f84837ea523928eeeb3ed44dbcf84a73d
kuqin <kuqin@microsoft.com>
2022-05-01 21:42:58 -0700

    Updated kmeans a bit to make it work better with the expected model

--- spike_cluster_kmeans.py
+++ spike_cluster_kmeans.py
@@ -7,7 +7,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import functions as F
 from math import sqrt
 
-MAX_ITER = 20
+MAX_ITER = 2
 
 class SpikeClusterKMeans(object):
 
@@ -19,7 +19,7 @@ class SpikeClusterKMeans(object):
   @staticmethod
   def e_dist (point_a, point_b):
     diff = point_a - point_b
-    ret = np.sum (diff * diff)
+    ret = diff * diff
     return ret
 
   @staticmethod
@@ -28,11 +28,12 @@ class SpikeClusterKMeans(object):
     new_cluster = None
     int_k = len (center)
     for cnt_idx in range (0, int_k):
-      measure = SpikeClusterKMeans.e_dist (center[cnt_idx], point)
+      ret = SpikeClusterKMeans.e_dist (center[cnt_idx], point)
+      measure = sum (ret)
       if min_dist == -1 or min_dist > measure:
         new_cluster = cnt_idx
         min_dist = measure
-    return (new_cluster, (min_dist, point))
+    return (new_cluster, (ret, point))
 
   # Implement k-means
   def KMeans(self, waveforms, k=3):
@@ -45,30 +46,39 @@ class SpikeClusterKMeans(object):
     std = [0] * k
     dev = [0] * k
 
-    data = self.sc.textFile(waveforms).map(lambda line: np.fromstring (line, sep=' '))
+    logging.debug ("Initialize RDD")
+    data = self.sc.parallelize(waveforms)
+    logging.debug ("Keamns entry")
 
     # 3: for iteration := 1 to MAX ITER do
-    for _ in range (0, MAX_ITER + 1):
+    for round in range (0, MAX_ITER + 1):
       # 4: for each point x in the dataset do
       # 5: Cluster of x ← the cluster with the closest centroid to x
       # 6: end for
+      logging.info ("Round %d, test point 1" % round)
       data_group = data.map (lambda a: SpikeClusterKMeans.eval_cnt (a, centroids))
 
       # 10: Calculate the cost for this iteration.
+      logging.info ("Round %d, test point 2" % round)
       cost_raw = data_group.mapValues(lambda a: a[0]).reduceByKey (lambda n1, n2: n1 + n2).collect()
+      logging.info ("Round %d, test point 3" % round)
       for idx, dist in cost_raw:
         dev[idx] = dist
 
       # 7: for each cluster P do
       # 8: Centroid of P ← the mean of all the data points assigned to P
       # 9: end for
-      mean = sorted(data_group.mapValues(lambda a: (a[1], 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).collect())
+      logging.info ("Round %d, test point 4" % round)
+      mean = data_group.mapValues(lambda a: (a[1], 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).collect()
+      logging.info ("Round %d, test point 5" % round)
       for idx, (total, count) in mean:
         centroids[idx] = total / count
         dev[idx] = dev[idx] / count
 
+      logging.debug ("Round %d complete" % round)
+
     for idx in range(k):
-      std[idx] = sqrt(dev[idx])
+      std[idx] = np.sqrt(dev[idx])
 
     # 11: end for
     return (centroids, std)
