4377122c735d76a357744a92f7ecbc6490b5c3fc
kuqin <kuqin@microsoft.com>
2022-05-01 21:58:32 -0700

    Well, this start to come together, but the K means performance is horrible

--- spike_main.py
+++ spike_main.py
@@ -1,7 +1,11 @@
 import argparse
+import logging
 import os
+import sys
+import wave
 import numpy as np
 import matplotlib.pyplot as plt
+from pyparsing import alphas
 from pyspark.sql import *
 
 import spike_filter_detect as sp_fd
@@ -10,6 +14,9 @@ from spike_cluster_kmeans import SpikeClusterKMeans
 from spike_fe import SpikeFeatureExtractPCA
 from spike_cluster_kmeans_mllib import SpikeClusterKMeans_MLLib
 from spike_fe_mllib import SpikeFeatureExtractPCA_MLLib
+
+path_root = os.path.dirname(__file__)
+sys.path.append(path_root)
 
 SAMPLE_FREQ = 30000
 
@@ -61,7 +68,7 @@ def main ():
   sc = spark.sparkContext
 
   # Mute the informational level logs
-  sc.setLogLevel("warn")
+  sc.setLogLevel("WARN")
 
   # This is Kun's home brew implementation
   skm = SpikeClusterKMeans (sc)
@@ -70,31 +77,42 @@ def main ():
   # # This is purely from spark MLLib
   # skm = SpikeClusterKMeans_MLLib (sc)
   # sfe = SpikeFeatureExtractPCA_MLLib (sc)
+  start_time = 0
 
   with open (Paths.InputFile, 'rb') as input:
-    # Detect spike, based on 
-    raw_data = input.read (Paths.Interval * SAMPLE_FREQ * Paths.Channels)
+    # Detect spike, the 2 factor is for the 16bit integer
+    raw_data = input.read (Paths.Interval * SAMPLE_FREQ * Paths.Channels * 2)
 
-    a = np.frombuffer(raw_data, dtype=np.uint16)
+    raw_data_16 = np.frombuffer(raw_data, dtype=np.int16)
+    timestamp = [(item / SAMPLE_FREQ) for item in range(start_time, start_time + SAMPLE_FREQ * Paths.Interval)]
 
     # TODO: Need to parallelize this
     for chn in range (Paths.Channels):
-      sliced_data = raw_data[chn::Paths.Channels]
+      sliced_data = raw_data_16[chn::Paths.Channels]
 
-      spike_data = sp_fd.filter_data(sliced_data, low=500, high=9000, sf=SAMPLE_FREQ)
-      plt.plot (wave_form)
+      spike_data = sp_fd.filter_data(sliced_data, low=300, high=6000, sf=SAMPLE_FREQ)
+      plt.plot(timestamp, spike_data)
+      plt.show ()
 
-      _, wave_form = sp_fd.get_spikes(spike_data, spike_window=50, tf=8, offset=20)
-      plt.plot (wave_form)
+      wave_form = sp_fd.get_spikes(spike_data, spike_window=50, tf=8, offset=20, max_thresh=500)
+      if len(wave_form) == 0:
+        # no spike here, bail this channel for this interval
+        continue
 
       # Now we are ready to cook. Start from feature extraction
+      logging.critical ("Start to process %d waveforms with PCA." % len(wave_form))
       extracted_wave = sfe.PCA (wave_form)
-      plt.plot (wave_form)
+      plt.plot (extracted_wave)
+      plt.show ()
+
+      logging.critical ("Done processing PCA!!!")
 
       cluster_means, cluster_std = skm.KMeans (extracted_wave, k=3)
+      fig, ax = plt.subplots(figsize=(15, 5))
       for idx in range (3):
-        plt.plot (cluster_means[idx], label = "line %d" % idx)
-        plt.fill_between (cluster_means[idx]-cluster_std[idx], cluster_means[idx]+cluster_std[idx])
+        ax.plot (range(len(cluster_means[idx])), cluster_means[idx], label = "line %d" % idx)
+        ax.fill_between (range(len(cluster_means[idx])), cluster_means[idx]-cluster_std[idx], cluster_means[idx]+cluster_std[idx], alpha=0.2)
+      plt.show ()
 
   return 0
 
