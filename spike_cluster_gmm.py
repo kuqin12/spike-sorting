e654a376edd49ccdf945d43201d33cdefa5aa6b7
kuqin <kuqin@microsoft.com>
2022-05-22 16:42:27 -0700

    Added reguralization inspired by sk implementation

--- spike_cluster_gmm.py
+++ spike_cluster_gmm.py
@@ -16,17 +16,18 @@ class SpikeClusterGMM(SpikeClustering):
     self.sp = spark
 
   # Implement GMM
-  def Cluster(self, waveforms, k=3, max_iter=20):
+  def Cluster(self, waveforms, k=3, max_iter=20, reg_cov=1e-6):
 
     # for initialization, we randomly select k vectors
     N = len(waveforms)
     pi = [1/k] * k
     mu = [None] * k
     sigma = [None] * k
+    reg_cov = 1e-6*np.identity(len(waveforms[0]))
     for idx in range (k):
       mu[idx] = random.choice(waveforms)
       sigma[idx] = np.zeros((len(waveforms[0]), len(waveforms[0])))
-      np.fill_diagonal(sigma[idx], np.cov(waveforms.T).diagonal())
+      np.fill_diagonal(sigma[idx], np.cov(waveforms.T).diagonal() + reg_cov)
 
     # Iterate towards the convergence
     for round in range (0, max_iter + 1):
@@ -34,7 +35,8 @@ class SpikeClusterGMM(SpikeClustering):
       # E-Step
       # Calculate probability belonging a specific cluster
       for cnt_idx in range (0, k):
-        distribution = multivariate_normal (mean=mu[cnt_idx], cov=sigma[cnt_idx], allow_singular=True)
+        cov = sigma[cnt_idx] + reg_cov
+        distribution = multivariate_normal (mean=mu[cnt_idx], cov=cov, allow_singular=True)
         prob[:,cnt_idx] = distribution.pdf(waveforms)
       weighted = prob * pi
       sum_weight = weighted.sum(axis=1)[:, np.newaxis]
@@ -50,7 +52,7 @@ class SpikeClusterGMM(SpikeClustering):
         mu[idx] = (waveforms * gamma_i).sum(axis=0) / sum_gamma_i
         # Update covariance based on points probablity in this cluster
         # TODO: KQ: This equation is wrong on the report, the transpose should be on the first x-u
-        sigma[idx] = np.dot((gamma_i*(waveforms-sum_gamma_i)).T,(waveforms-sum_gamma_i))/sum_gamma_i
+        sigma[idx] = np.dot((gamma_i*(waveforms-sum_gamma_i)).T,(waveforms-sum_gamma_i))/sum_gamma_i + reg_cov
 
     # Collect the result by looking up the maximal probability
     clusters = [None] * k
