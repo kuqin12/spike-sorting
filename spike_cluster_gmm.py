Fixed the matrix calculation errors..

spike_cluster_gmm.py
@@ -6,23 +6,14 @@ import random
 from pyspark.sql import *
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import functions as F
-from math import gamma, sqrt
+# from math import gamma, sqrt
+from scipy.stats import multivariate_normal
 from spike_cluster_type import SpikeClustering
 
 class SpikeClusterGMM(SpikeClustering):
 
   def __init__(self, spark):
     self.sp = spark
-
-  # Helper function to calculate euclidean distance
-  # Note that this is square as is!!!
-  @staticmethod
-  def e_prob (point, mu_i, sigma_i, k):
-    mid = np.matmul ((point - mu_i).T, np.linalg.inv(sigma_i))
-    order = -np.matmul (mid, (point - mu_i)) / 2
-    det = np.linalg.det (sigma_i)
-    ret = np.exp (order) / np.sqrt (np.power((2*np.pi), k) * det)
-    return ret
 
   @staticmethod
   def eval_cnt (point, mu, pi, sigma):
@@ -31,7 +22,9 @@ class SpikeClusterGMM(SpikeClustering):
     int_k = len (pi)
     gamma = np.array([0] * int_k, dtype = float)
     for cnt_idx in range (0, int_k):
-      ret = SpikeClusterGMM.e_prob (point, mu[cnt_idx], sigma[cnt_idx], int_k)
+      distribution = multivariate_normal (mean=mu[cnt_idx], cov=sigma[cnt_idx], allow_singular=True)
+      ret = distribution.pdf(point)
+      # ret = SpikeClusterGMM.e_prob (point, mu[cnt_idx], sigma[cnt_idx], int_k)
       if max_prob < ret:
         new_cluster = cnt_idx
         max_prob = ret
@@ -39,55 +32,55 @@ class SpikeClusterGMM(SpikeClustering):
     gamma = gamma / sum (gamma)
     return (new_cluster, point, gamma)
 
-  @staticmethod
-  def update_cent (entry, center):
-    idx = entry[0]
-    total = entry[1][0]
-    count = entry[1][1]
-    center[idx] = total/count
-    return entry
-
   # Implement GMM
   def Cluster(self, waveforms, k=3, max_iter=20):
 
     # for initialization, we randomly select k vectors
     N = len(waveforms)
-    pi = [1/N] * k
+    pi = [1/k] * k
     mu = [None] * k
     sigma = [None] * k
     for idx in range (k):
       mu[idx] = random.choice(waveforms)
-      sigma[idx] = np.abs(np.cov (waveforms.T))
+      sigma[idx] = np.zeros((len(waveforms[0]), len(waveforms[0])))
+      np.fill_diagonal(sigma[idx], np.cov(waveforms.T).diagonal())
 
     # Iterate towards the convergence
     for round in range (0, max_iter + 1):
-      clusters = [ [] for _ in range(k) ]
+      prob = np.zeros ((N, k))
       # E-Step
-      for idx in range (len(waveforms)):
-        # Assign to the cluster with the largest value of gamma
-        res = SpikeClusterGMM.eval_cnt (waveforms[idx], mu, pi, sigma)
-        clusters[res[0]].append ((idx, res[1], res[2]))
+      # Calculate probability belonging a specific cluster
+      for cnt_idx in range (0, k):
+        distribution = multivariate_normal (mean=mu[cnt_idx], cov=sigma[cnt_idx], allow_singular=True)
+        prob[:,cnt_idx] = distribution.pdf(waveforms)
+      weighted = prob * pi
+      sum_weight = weighted.sum(axis=1)[:, np.newaxis]
+      gamma = weighted / sum_weight # same shape as prob: N x k
 
       # M-Step
       for idx in range(k):
-        sum_d = 0
-        sum_s = np.array ([[0] * len(waveforms[0])] * len(waveforms[0]), dtype=float)
-        sum_n = 0
-        cluster = clusters[idx]
+        gamma_i = gamma[:, [idx]]
+        sum_gamma_i = sum (gamma[:, idx])
         # Fraction of number of points
-        pi[idx] = len (cluster) / N
-        for idx_2 in range(k):
-          sum_d += sum(point * gamma[idx] for _, point, gamma in clusters[idx_2])
-          sum_s += sum(np.dot (point[:,None], [point]) * gamma[idx] for _, point, gamma in clusters[idx_2])
-          sum_n += sum(point * gamma[idx] for _, point, gamma in clusters[idx_2])
+        pi[idx] = sum_gamma_i / N
         # Update means based on the points in this cluster
-        mu[idx] = sum_d / sum_n
-        # Update covariance based on points in this cluster
-        sigma[idx] = sum_s / sum_n
+        mu[idx] = (waveforms * gamma_i).sum(axis=0) / sum_gamma_i
+        # Update covariance based on points probablity in this cluster
+        # TODO: KQ: This equation is wrong on the report, the transpose should be on the first x-u
+        sigma[idx] = np.dot((gamma_i*(waveforms-sum_gamma_i)).T,(waveforms-sum_gamma_i))/sum_gamma_i
 
-    # Collect the result from the last iteration
-    for idx in range(k):
-      clusters[idx] = [i for i, _, _ in clusters[idx]]
+    # Collect the result by looking up the maximal probability
+    clusters = [None] * k
+    prob = np.zeros ((N, k))
+    for cnt_idx in range (k):
+      distribution = multivariate_normal (mean=mu[cnt_idx], cov=sigma[cnt_idx], allow_singular=True)
+      prob[:,cnt_idx] = distribution.pdf(waveforms)
+    max_index = prob.argmax(axis=1)
+    for idx in range(N):
+      if clusters[max_index[idx]] is None:
+        clusters[max_index[idx]] = [idx]
+      else:
+        clusters[max_index[idx]].append(idx)
 
     # 11: end for
     return clusters